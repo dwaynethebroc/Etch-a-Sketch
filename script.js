3b090247cec2813ec89e3fc420dbcc972386b9b7
dwaynethebroc <127725696+dwaynethebroc@users.noreply.github.com>
2023-07-22 20:18:16 +0800

    create file, start of div

---
+++ script.js
@@ -0,0 +1,20 @@
+let grid = document.getElementById("grid");
+let cell = document.createElement("div");
+
+for (i = 0; i <= 16; i++){
+
+    let row = document.createElement("div");
+    row.backgroundColor = "red";
+    row.classList.add(`row`);
+    grid.appendChild(row);
+
+    for(j=0; j <= 16; j++){
+        cell = document.createElement("div");
+        cell.innerText = `div \n ${j}`
+        cell.style.backgroundColor = "orange";
+        cell.classList.add(`cell`);
+        row.appendChild(cell);
+    }
+
+
+}
