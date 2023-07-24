3cfba71f96cffbbfa1f8d827e09824b1b9e720c3
dwaynethebroc <127725696+dwaynethebroc@users.noreply.github.com>
2023-07-24 12:54:01 +0800

    gridBuild works, grid change works, grid clear:(

--- script.js
+++ script.js
@@ -1,24 +1,75 @@
 let grid = document.getElementById("grid");
+let row = document.getElementsByClassName("row");
 let cell = document.getElementsByClassName("cell");
+let button = document.getElementById("numberOfSquares");
+let gridSize = 16;
+let input;
+let response;
+
+button.addEventListener("click", changeGridSize);
 
 function changeColor() {
     this.classList.add('hovered');
-    
 }
 
-
-for (i = 1; i <= 16; i++){
-
-    let row = document.createElement("div");
-    row.classList.add(`row`);
-    grid.appendChild(row);
-
-    for(j=1; j <= 16; j++){
-        cell = document.createElement("div");
-        cell.classList.add(`cell`);
-        cell.addEventListener("mouseover", changeColor);
-        row.appendChild(cell);
+function buildGrid(gridSize) {
+    for (i = 1; i <= gridSize; i++){
+        row = document.createElement("div");
+        row.classList.add(`row`);
+        grid.appendChild(row);
+    
+        for(j=1; j <= gridSize; j++){
+            cell = document.createElement("div");
+            cell.classList.add(`cell`);
+            cell.addEventListener("mouseover", changeColor);
+            row.appendChild(cell);
+        }
     }
 }
 
+function clearGrid(gridSize){
+    for (i = gridSize; i > 0; i--){
+        row.classList.remove('row');
+        grid.removeChild(row);
+
+        for(j = gridSize; j > 0; j--){
+            cell.classList.remove('cell');
+            cell.removeEventListener("mouseover", changeColor);
+            row.removeChild(cell);
+        }
+    }
+    
+}
+
+function changeGridSize(){
+    response = prompt('Enter the number of squares on the grid you would like to see? Please only include a number between 1-100.', '');
+    console.log(response);
+    input = Number(response);
+    console.log(typeof(input));
+    console.log(input);
+
+    while(input > 100 && input < 1 && input !== typeof(Number)){
+        changeGridSize();
+    }
+
+    gridSize = input;
+    clearGrid(gridSize);
+
+        
+    for (i = 1; i <= input; i++){
+
+        let row = document.createElement("div");
+        row.classList.add(`row`);
+        grid.appendChild(row);
+
+        for(j=1; j <= input; j++){
+            cell = document.createElement("div");
+            cell.classList.add(`cell`);
+            cell.addEventListener("mouseover", changeColor);
+            row.appendChild(cell);
+        }
+    }
+}
+
+buildGrid(gridSize);
 
